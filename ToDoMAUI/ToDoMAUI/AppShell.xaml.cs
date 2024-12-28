3fd1bdd04adca7a93031647c2beec8f7a0cbc8b7
Geisson Oliveira <geissonlucaso@gmail.com>
2024-12-28 17:49:02 -0300

    Create the project.

---
+++ ToDoMAUI/ToDoMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ToDoMAUI/ToDoMAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
